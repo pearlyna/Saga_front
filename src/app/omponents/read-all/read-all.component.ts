51891fecdeca409de40759ffe33e3355e469d237
serenvila <serenvilar.br@gmail.com>
2024-11-18 10:27:44 -0300

    subindo

---
+++ src/app/omponents/read-all/read-all.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-read-all',
+  imports: [],
+  templateUrl: './read-all.component.html',
+  styleUrl: './read-all.component.scss'
+})
+export class ReadAllComponent {
+
+}
